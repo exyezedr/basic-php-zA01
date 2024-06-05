53d0051f67fe7738b286bc29ed020821b9b00bc6
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-06-05 08:33:10 +0700

    add 04-null/src/01.php

---
+++ 04-null/src/01.php
@@ -0,0 +1,22 @@
+<?php
+
+$name = "john doe";
+$name = null;
+
+echo json_encode($name) . "\n";
+echo json_encode(is_null($name)) . "\n";
+echo json_encode(isset($name)) . "\n";
+
+unset($name);
+
+echo json_encode(isset($name)) . "\n";
+
+/**
+ * error
+ * ==================================================
+ * echo json_encode(is_null($name)) . "\n";
+ */
+
+$name = "john doe";
+
+echo json_encode(isset($name)) . "\n";
