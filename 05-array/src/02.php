7dc7aa5ca8faf5e8dde0ffa77791da93a4164307
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-06-05 08:38:48 +0700

    add 05-array/src/02.php

---
+++ 05-array/src/02.php
@@ -0,0 +1,18 @@
+<?php
+
+$fruits = ["apple", ["banana", "watermelon"], "orange"];
+
+echo count($fruits) . "\n";
+echo "{$fruits[1][0]}\n";
+
+$fruits[] = "grape";
+
+echo json_encode($fruits) . "\n";
+
+$fruits[1][1] = "pineapple";
+
+echo json_encode($fruits) . "\n";
+
+unset($fruits[1]);
+
+echo json_encode(array_values($fruits)) . "\n";
