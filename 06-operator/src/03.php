801dcd816be9b15fd4f97b7110aca6dd29c5bbe3
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-06-05 08:45:08 +0700

    add 06-operator/src/03.php

---
+++ 06-operator/src/03.php
@@ -0,0 +1,11 @@
+<?php
+
+echo json_encode(10 == "10") . "\n";
+echo json_encode(10 != "10") . "\n";
+echo json_encode(10 <> "10") . "\n";
+echo json_encode(10 === "10") . "\n";
+echo json_encode(10 !== "10") . "\n";
+echo json_encode(10 > "10") . "\n";
+echo json_encode(10 < "10") . "\n";
+echo json_encode(10 <= "10") . "\n";
+echo json_encode(10 >= "10") . "\n";
