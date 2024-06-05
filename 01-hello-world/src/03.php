a795c46c6b7f9ade22305287d23a750cb29c3157
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-06-05 12:21:20 +0700

    add 01-hello-world/src/03.php

---
+++ 01-hello-world/src/03.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * documentation
+ * calculate the sum of two integer
+ *
+ * @param int $a the first integer to be add
+ * @param int $b the second integer to be add
+ * @return int the sum of $a and $b
+ */
+function sum(int $a, int $b): int
+{
+    return $a + $b;
+}
+
+echo sum(10, 10) . "\n";
+
+/*
+multi line
+echo "this code will not be execute\n";
+*/
+echo "hello world\n";
+
+// single line
+// echo "this code will not be execute\n";
+echo "hello world\n";
