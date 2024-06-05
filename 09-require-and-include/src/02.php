b2b6e28fcdc5e9bb27df9c793d0df1c07b3c4c81
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-06-05 12:25:47 +0700

    add 09-require-and-include/src/02.php

---
+++ 09-require-and-include/src/02.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * if there is an error in the path, the program will stop
+ * may cause a redeclare function, if it call the same file
+ */
+require __DIR__ . "/my-function.php";
+
+echo 10 + 10 . "\n";
+echo sayHello("john doe") . "\n";
