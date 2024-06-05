f7655160cc262fc7b6a899d51abb8682e373fd79
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-06-05 10:45:56 +0700

    add 06-control-flow/src/09.php

---
+++ 06-control-flow/src/09.php
@@ -0,0 +1,30 @@
+<?php
+
+// without for each
+$fruits = ["apple", "banana", "orange"];
+
+for ($i = 0; $i < count($fruits); $i++) {
+    echo "$fruits[$i]\n";
+}
+
+echo "==================================================\n";
+
+foreach ($fruits as $fruit) {
+    echo "$fruit\n";
+}
+
+echo "==================================================\n";
+
+foreach ($fruits as $index => $fruit) {
+    echo "$index $fruit\n";
+}
+
+echo "==================================================\n";
+
+$user = ["first_name" => "john", "last_name" => "doe"];
+
+foreach ($user as $key => $name) {
+    $key = str_replace("_", " ", $key);
+
+    echo "my $key is $name\n";
+}
