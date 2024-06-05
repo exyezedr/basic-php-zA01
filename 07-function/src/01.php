deb6ab5948e646609ef49d1b7cdd1f7b5f026840
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-06-05 11:49:59 +0700

    add 07-function/src/01.php

---
+++ 07-function/src/01.php
@@ -0,0 +1,19 @@
+<?php
+
+function sayHello(): void
+{
+    echo "hello\n";
+
+    $create = true;
+
+    if ($create) {
+        function sayHi(): void
+        {
+            echo "hi\n";
+        }
+    }
+
+    sayHi();
+}
+
+sayHello();
