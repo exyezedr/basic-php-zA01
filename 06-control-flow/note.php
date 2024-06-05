61d06e8aa329d0f34ca24873537155a6d7dbc770
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-06-05 10:54:30 +0700

    add 06-control-flow/note.php

---
+++ 06-control-flow/note.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * 01. if
+ * 02. switch
+ * 03. ternary
+ * 04. null coalescing
+ * 05. for
+ * 06. while
+ * 07. do while
+ * 08. break and continue
+ * 09. for each
+ * 10. go to
+ */
