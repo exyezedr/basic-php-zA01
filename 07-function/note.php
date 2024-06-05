277ae066d498f7489d6ee63c87fe4e6e26638ab3
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-06-05 12:17:56 +0700

    add 07-function/note.php

---
+++ 07-function/note.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * 01. function
+ * 02. parameter and argument
+ * 03. return
+ * 04. variable function
+ * 05. anonymous
+ * 06. arrow
+ * 07. call user function
+ * 08. recursive
+ */
