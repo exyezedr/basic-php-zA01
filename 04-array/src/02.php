53bddd8c3f77c54743ba7fe7a9fcb612b94c386e
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-06-10 09:18:18 +0700

    update 04-array/src/02.php

--- 04-array/src/02.php
+++ 04-array/src/02.php
@@ -6,13 +6,10 @@ echo count($fruits) . "\n";
 echo "{$fruits[1][0]}\n";
 
 $fruits[] = "grape";
-
 echo json_encode($fruits) . "\n";
 
 $fruits[1][1] = "pineapple";
-
 echo json_encode($fruits) . "\n";
 
 unset($fruits[1]);
-
 echo json_encode(array_values($fruits)) . "\n";
